acf7c01b9abffa62965720e33b341b1f76433be5
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-06-16 14:45:54 -0300

    tests: add performance-test get and delete methods

--- src/test/java/com/example/api/performance/PerformanceSimulation.java
+++ src/test/java/com/example/api/performance/PerformanceSimulation.java
@@ -7,7 +7,12 @@ import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import java.time.Duration;
 
-import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.core.CoreDsl.StringBody;
+import static io.gatling.javaapi.core.CoreDsl.jsonPath;
+import static io.gatling.javaapi.core.CoreDsl.rampUsersPerSec;
+import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.core.CoreDsl.global;
 import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.status;
 
@@ -15,30 +20,68 @@ public class PerformanceSimulation extends Simulation {
     private final HttpProtocolBuilder httpProtocolBuilder = http.baseUrl("http://localhost:8080")
             .header("Content-Type", "application/json");
 
-    ActionBuilder addRequestMessage = http("add message")
+    ActionBuilder addMessageRequest = http("request: add message")
             .post("/messages")
             .body(StringBody("{ \"username\": \"John\", \"content\": \"Hello, World!\" }"))
-            .check(status().is(201));
+            .check(status().is(201))
+            .check(jsonPath("$.id").saveAs("messageId"));
+
+    ActionBuilder getMessageRequest = http("request: get message")
+            .get("/messages/#{messageId}")
+            .check(status().is(200));
+
+    ActionBuilder deleteMessageRequest = http("request: delete message")
+            .delete("/messages/#{messageId}")
+            .check(status().is(200));
+
 
     ScenarioBuilder scenarioBuilderAddMessage = scenario("Add message")
-            .exec(addRequestMessage);
+            .exec(addMessageRequest);
+
+    ScenarioBuilder scenarioBuilderGetMessage = scenario("Get message")
+            .exec(addMessageRequest)
+            .exec(getMessageRequest);
+
+    ScenarioBuilder scenarioBuilderDeleteMessage = scenario("Delete message")
+            .exec(addMessageRequest)
+            .exec(deleteMessageRequest);
 
     {
-        setUp(scenarioBuilderAddMessage.injectOpen(
-            rampUsersPerSec(1)
-                    .to(10)
-                    .during(Duration.ofSeconds(10)),
-            constantUsersPerSec(10)
-                    .during(Duration.ofSeconds(20)),
-            rampUsersPerSec(10)
-                    .to(1)
-                    .during(Duration.ofSeconds(10))
-
-            )
+        setUp(
+                scenarioBuilderAddMessage.injectOpen(
+                    rampUsersPerSec(1)
+                            .to(2)
+                            .during(Duration.ofSeconds(10)),
+                    constantUsersPerSec(2)
+                            .during(Duration.ofSeconds(20)),
+                    rampUsersPerSec(2)
+                            .to(1)
+                            .during(Duration.ofSeconds(10))
+                ),
+                scenarioBuilderGetMessage.injectOpen(
+                        rampUsersPerSec(1)
+                                .to(10)
+                                .during(Duration.ofSeconds(10)),
+                        constantUsersPerSec(10)
+                                .during(Duration.ofSeconds(20)),
+                        rampUsersPerSec(10)
+                                .to(1)
+                                .during(Duration.ofSeconds(10))
+                ),
+                scenarioBuilderDeleteMessage.injectOpen(
+                        rampUsersPerSec(1)
+                                .to(5)
+                                .during(Duration.ofSeconds(10)),
+                        constantUsersPerSec(5)
+                                .during(Duration.ofSeconds(20)),
+                        rampUsersPerSec(5)
+                                .to(1)
+                                .during(Duration.ofSeconds(10))
+                )
         )
             .protocols(httpProtocolBuilder)
             .assertions(
-                    global().responseTime().max().lt(300)
+                    global().responseTime().max().lt(5000)
             );
     }
 }
